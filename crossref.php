b748dd840e5d0014d0ac3f7c665a71d1575dfc58
The research fairy <bgcarlisle@users.noreply.github.com>
2025-10-01 12:02:39 -0400

    Crossref PHP now returns JSON for provided DOI

--- crossref.php
+++ crossref.php
@@ -1,13 +1,43 @@
 <?php
 
-$result = file_get_contents('https://api.crossref.org/works/' . urlencode('10.1016/j.medj.2024.07.014'), false, null);
+header('Content-Type: application/json');
 
-// var_dump($http_response_header);
-
-var_dump($result);
+$result = file_get_contents('https://api.crossref.org/works/' . urlencode($_GET['doi']), false, null);
 
 $json = json_decode($result);
 
-echo "<p>Title: " . $json->{'message'}->{'title'}[0] . "</p>";
+$title = $json->{'message'}->{'title'}[0];
+
+$authors = implode(
+    ", ",
+    array_map(
+	fn($a) => $a->{'family'} . " " . implode('', array_map( fn($fname) => substr($fname, 0, 1), explode(' ', $a->{'given'}))), $json->{'message'}->{'author'}
+    )
+);
+
+$journal = $json->{'message'}->{'container-title'}[0];
+
+$year = $json->{'message'}->{'journal-issue'}->{'published-print'}->{'date-parts'}[0][0];
+
+if (is_null($year)) {
+    $year = $json->{'message'}->{'published-online'}->{'date-parts'}[0][0];
+}
+
+$doi = $json->{'message'}->{'URL'};
+
+// var_dump($result);
+
+// echo "<p>" . $authors . ". " . $title . ". " . $journal . ", ", $year . ". " . $doi . "</p>";
+
+echo json_encode(
+    array(
+ 	"authors" => $authors,
+ 	"title" => $title,
+ 	"journal" => $journal,
+ 	"year" => $year,
+ 	"doi" => $doi
+    )
+);
+
 
 ?>
